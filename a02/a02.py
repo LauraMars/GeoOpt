550833d6a49603b8e54f1a47294f96d4fb740ad7
LauraMars <80417739+LauraMars@users.noreply.github.com>
2021-05-10 19:11:30 +0200

    UPDATE_SUPPORT WITH DAI

--- a02/a02.py
+++ a02/a02.py
@@ -94,8 +94,91 @@ d = exploded
 #the result should be a mesh that responds to the sun position... its up to you!
 
 
-# I am trying to move the mesh faces in relation to the sun vector and scale them in relation to the angle value 
 
+
+# I am trying to move the mesh faces in relation to the sun vector and scale them in relation to the angle value 
+#SUPPORT WITH DAI
+
+#0 Remap angle values to control the range of the scale
+remaped_angles=[]
+min_offset=min_off
+max_offset=max_off #reverse
+for i in angleList:
+    remaped = ( (i - min(angleList)) / (max(angleList) - min(angleList)) ) * (max_offset - min_offset) + min_offset
+    remaped_angles.append(remaped)
+
+
+#1.move faces in relation to sun vector
+moved_faces=[]
+
+for i in range(len(exploded)):
+    face=rg.Mesh.Duplicate(exploded[i])
+    vector = s+rg.Vector3d(a[i])/200
+    moved=rg.Mesh.Transform(face,rg.Transform.Translation(-vector/8)) #gives true/false
+    moved_faces.append(face)
+
+
+
+#2.scale faces in relation to angle value
+#2.1 Get faces outlines
+moved_outlines=[]
+for i in moved_faces:
+    FaceOutlines=rg.Mesh.GetNakedEdges(i)
+    moved_outlines.append(FaceOutlines)
+
+
+#2.2 Convert to curves and Join
+joined_moved=[]
+for i in range(len(moved_outlines)):
+    curves=[]
+    for j in range(len(moved_outlines[i])):
+        #convert to nurbs curve
+        curve=rg.Polyline.ToNurbsCurve(moved_outlines[i][j])
+        curves.append(curve)
+    joined_curves=rg.NurbsCurve.JoinCurves(curves)[0]
+    joined_moved.append(joined_curves)
+
+
+#2.3 Offset joined outlines
+offset_outlines=[]
+for i in range(len(joined_moved)):
+    #convert mesh to surface
+    face_points=rg.Mesh.Vertices.GetValue(moved_faces[i])
+    surface=rg.NurbsSurface.CreateFromPoints([rg.Point3d(pt) for pt in face_points],2,2,2,2)#list_comprehension 
+    offseted=rg.Curve.OffsetOnSurface(joined_moved[i],surface,remaped_angles[i],.001)[0]
+    offset_outlines.append(offseted)
+#########################
+outlines=[]
+for i in exploded:
+    FaceOutlines=rg.Mesh.GetNakedEdges(i)
+    outlines.append(FaceOutlines)
+joined=[]
+for i in range(len(outlines)):
+    curves=[]
+    for j in range(len(outlines[i])):
+        #convert to nurbs curve
+        curve=rg.Polyline.ToNurbsCurve(outlines[i][j])
+        curves.append(curve)
+    joined_curves=rg.NurbsCurve.JoinCurves(curves)[0]
+    joined.append(joined_curves)
+
+
+#2.4 Loft 
+panels_with_openings=[]
+for i in range(len(offset_outlines)):
+    curves_list=[offset_outlines[i],joined[i]]
+    lofted=rg.Brep.CreateFromLoft(curves_list,rg.Point3d.Unset,rg.Point3d.Unset,rg.LoftType.Normal,False)[0]
+    panels_with_openings.append(lofted)
+
+
+
+
+
+
+
+
+
+"""
 move_point=[]
 
 for i in centers:
@@ -129,8 +212,5 @@ f = th.list_to_tree(AllVert)
 
 #move face
 """
-Move_faces=[]
-for j in exploded:
-    face_move=[]
 
-"""
+
